2b5baa6bd6502ad6f4f73a64d2f2759ef8e16887
Dean <deanapesek99@gmail.com>
2019-06-26 09:21:03 -0600

    creating the repo

---
+++ src/main/java/pesek/caruso/echoserver/EchoClient.java
@@ -0,0 +1,10 @@
+package pesek.caruso.echoserver;
+
+public class EchoClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/pesek/caruso/echoserver/EchoServer.java
@@ -0,0 +1,10 @@
+package pesek.caruso.echoserver;
+
+public class EchoServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
